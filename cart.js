8bc47fd6acca1ff9335e3b2dddefa360dc8f6d8e
ColorizeMySky <firiziant@gmail.com>
2018-10-08 02:48:26 +0300

    Complete basic html + css

--- cart.js
+++ cart.js
@@ -11,3 +11,57 @@ function equalHeightImg() {
 	});
 	$('.wrapper-img').height(height);
 }
+
+/*
+$('.for-more').on('click', playForward);
+
+function playForward() {
+	$(this).parent().addClass('change-view');
+	$(this).siblings('.shadow-for-holder').addClass('invisible');
+	$(this).siblings('.backside').removeClass('invisible');
+	$(this).delay(1500).queue(function (){
+		$(this).text('Back');
+		$(this).removeClass('for-more');
+		$(this).addClass('for-back');
+		$(this).on('click', playBack);
+	});
+}
+function playBack() {
+	$(this).parent().removeClass('change-view');
+	$(this).parent().addClass('change-view-back');
+	$(this).siblings('.backside').addClass('invisible');
+	$(this).text('Click for details');
+	$(this).delay(2000).queue(function (){
+		$(this).siblings('.shadow-for-holder').removeClass('invisible');
+		$(this).removeClass('for-back');
+		$(this).addClass('for-more');
+		//$(this).on('click', playForward);
+		$('.for-more').on('click', playForward);
+	});
+}
+*/
+
+$('.details').on('click', function() {
+	if ($(this).parent().hasClass('change-view-back') || $(this).parent().hasClass('change-view')) {
+		$(this).parent().toggleClass('change-view');
+		$(this).parent().toggleClass('change-view-back');
+	}
+	else {
+		$(this).parent().addClass('change-view');
+	}
+
+	if ($(this).siblings('.shadow-for-holder').hasClass('invisible')) {
+		$(this).siblings('.shadow-for-holder').delay(3000).queue(function (){
+			$(this).removeClass('invisible');
+			$(this).siblings('.details').text('Click for details');
+			console.log($(this).text());
+		});
+	}
+	else {
+		$(this).siblings('.shadow-for-holder').addClass('invisible');
+		$(this).text('Back');
+		console.log($(this).text());
+	}
+
+	$(this).siblings('.backside').toggleClass('invisible');
+});
